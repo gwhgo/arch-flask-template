1c31cb6bd71b27beae7d0b9a39d6e0dd12f339b5
William Guo <williamguogo@gmail.com>
2020-09-11 06:15:34 +0800

    add: gitignore;content: venv

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
 from app import app
+from flask import render_template
 
 @app.route("/")
+@app.route('/index')
 def index():
-    return "index"
+    user = {'usenrmae':'Miguel'}
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body': 'The avengers movie is so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
